9ca6471bf3fbb7e2aff71bfe4c92add28bf8119b
SevketYALCIN <sevket.yalcin@outlook.com>
2018-07-03 18:53:54 +0900

    feat: favicon + manifest

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,6 +5,7 @@ import * as github from "./github.png"
 import "./layout.scss"
 import * as mail from "./mail.png"
 import * as twitter from "./twitter.png"
+import { Helmet } from "react-helmet";
 
 class Template extends React.Component<BaseTemplateProps> {
   render() {
@@ -47,6 +48,19 @@ class Template extends React.Component<BaseTemplateProps> {
     
     return (
       <div className="layout">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          
+          {/* Favicon stuff from realfavicongenerator.net */}
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+          <link rel="manifest" href="/site.webmanifest"/>
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+          <meta name="msapplication-TileColor" content="#da532c"/>
+          <meta name="theme-color" content="#ffffff"/>
+        </Helmet>
         {header}
         {children}
       </div>
