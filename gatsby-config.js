1e4f907adefc4f8da49ed9cbbb53acab43f2cee6
Sevket <sevket.yalcin@outlook.com>
2018-07-14 15:52:46 +0900

    feat: add analytics tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,7 +42,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-122340394-1`,
       },
     },
     `gatsby-plugin-feed`,
