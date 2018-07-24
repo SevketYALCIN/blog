e7066a217930bf211f6b5807e2a67a8939ff838a
Sevket <sevket.yalcin@outlook.com>
2018-07-24 21:39:46 +0900

    Update layout.tsx

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -71,6 +71,7 @@ class Template extends React.Component<BaseTemplateProps> {
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
           <link rel="manifest" href="/site.webmanifest"/>
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+          <meta name="google-site-verification" content="0pdQzr72UjO22IoTTpMvgQWwyrms9LKpQuzu7pBdNS0" />
           <meta name="msapplication-TileColor" content="#da532c"/>
           <meta name="theme-color" content="#ffffff"/>
         </Helmet>
