35560a16c32e96dbaf14c62884e070b578545cd0
Sevket YALCIN <sevket.yalcin@outlook.com>
2018-10-03 16:39:39 +0900

    fix: remove border bottom on h1 globally

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,30 +2,32 @@ import Typography from 'typography'
 import githubTheme from 'typography-theme-github'
 
 githubTheme.overrideThemeStyles = () => ({
-  'body': {
-    background: "#fbfafc"
+  body: {
+    background: '#fbfafc',
   },
   '.layout :not(pre) > code[class*="language-"]': {
-    padding: "3px .1em",
-    borderRadius: "0"
+    padding: '3px .1em',
+    borderRadius: '0',
   },
-  
-'::selection': {
-  background: '#ffb7b7' /* WebKit/Blink Browsers */
-},
-'::-moz-selection': {
-  background: '#ffb7b7' /* Gecko Browsers */
-},
-'a:not(.special-link)': {
-  borderBottom: '1px dotted #98333359',
-  textDecoration: 'none',
-  color: 'black',
-  transition: 'color 0.5s ease'
-},
-'a:not(.special-link):hover': {
-  color: '#d04444'
-}
 
+  '::selection': {
+    background: '#ffb7b7' /* WebKit/Blink Browsers */,
+  },
+  '::-moz-selection': {
+    background: '#ffb7b7' /* Gecko Browsers */,
+  },
+  'a:not(.special-link)': {
+    borderBottom: '1px dotted #98333359',
+    textDecoration: 'none',
+    color: 'black',
+    transition: 'color 0.5s ease',
+  },
+  'a:not(.special-link):hover': {
+    color: '#d04444',
+  },
+  'h1': {
+    borderBottom: 'none'
+  }
 })
 
 const typography = new Typography(githubTheme)
